7c5ab89bc1cf553b29cd558ed5c4b90c7d782493
ArturoCuellar <arturocuellarm3@gmail.com>
2022-01-19 23:36:52 -0600

    Manipulacion del DOM Terminado

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+  //Ejercicio 1
   function js_style(){
     document.getElementById("text").style.color="red";
     document.getElementById("text").style.fontSize="40px";
@@ -5,7 +6,7 @@
     document.getElementById("text").style.backgroundColor="black";
     }
 
-
+//Ejercicio 2
     function getFormvalue(){
         var first = document.getElementById("fname").value;
         var second = document.getElementById("lname").value;
@@ -13,7 +14,7 @@
       console.log(first);
       console.log(second);
     }
-
+//Ejercicio 3
     function numberlinks(){
         alert("Total de links: "+ document.body.getElementsByTagName("a").length);
 alert("Primer Link: "+document.body.getElementsByTagName("a")[0]);
@@ -23,7 +24,32 @@ alert("Ultimo link: "+document.body.getElementsByTagName("a")[2]);
     }
 
 
+//Ejercicio 4
 
+function selectContainer(){
+
+  document.getElementById("container").style.background="red";
+
+  document.querySelector("#container").style.background="blue";
+
+  let x = document.getElementsByClassName("second");
+  console.log(x);
+
+  let a = document.querySelector("ol .third");
+  console.log(a);
+
+  document.querySelector("#container").innerHTML ="Hello Arturo";
+
+  document.querySelector(".footer").classList.remove("main");
+
+  let li= document.createElement("li");
+
+  li.innerText = "four";
+  document.querySelector("ul").appendChild(li);
+
+}
+
+selectContainer();
 
 
    
